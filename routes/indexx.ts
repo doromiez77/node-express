04350b2820f0bb5fa288526eb5af13d1748d89bb
이태겸 <tglee@deepscent.io>
2023-11-27 15:50:36 +0900

    test2

--- routes/indexx.ts
+++ routes/indexx.ts
@@ -3,6 +3,7 @@ let isAdult: boolean = true;
 let a: number[] = [1, 2, 3];
 let a2: Array<number> = [1, 2, 3];
 let a4: number[] = [1, 2, 3, 5];
+let a6: Array<number> = [1, 2, 3];
 
 let week1: string[] = ["mon", "tue", "wed"];
 let week2: Array<string> = ["mon", "tue", "wed"];
